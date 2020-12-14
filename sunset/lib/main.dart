6c1834ecc258429830b188b62b88c3ae3988a7f8
Akash Devdhar <neospeed83@gmail.com>
2020-12-13 18:04:11 -0700

    Added location detected and local sunset time information

--- sunset/lib/main.dart
+++ sunset/lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:intl/intl.dart';
+import 'package:geocoder/geocoder.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -12,9 +18,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-        
       ),
-      home: MyHomePage(title: 'Sunset'),
+      home: MyHomePage(title: 'Sunset Notifier'),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -30,11 +35,62 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  double lat;
+  double lon;
+  DateTime data;
+  String address;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    getCurrentLocation();
+  }
+
+  getCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.best,
+    );
+
     setState(() {
-      _counter++;
+      lat = position.latitude;
+      lon = position.longitude;
+    });
+
+    final coordinates = new Coordinates(lat, lon);
+    var addresses =
+        await Geocoder.local.findAddressesFromCoordinates(coordinates);
+
+    var first = addresses.first;
+    setState(() {
+      address = "${first.addressLine}";
+    });
+    getSunsetData();
+  }
+
+  getSunsetData() async {
+    Uri uri = Uri(
+      scheme: 'https',
+      host: 'api.sunrise-sunset.org',
+      path: '/json',
+      queryParameters: {
+        'lat': lat.toString(),
+        'lng': lon.toString(),
+        'formatted': '0'
+      },
+    );
+
+    http.Response response;
+    try {
+      response = await http.get(uri);
+    } on Exception catch (e) {
+      print('Network Error: $e');
+      throw e;
+    }
+
+    final jsonResponse = json.decode(response.body);
+
+    setState(() {
+      data = DateTime.parse(jsonResponse["results"]["sunset"]);
     });
   }
 
@@ -47,17 +103,45 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            data == null
+                ? CircularProgressIndicator()
+                : Column(
+                    children: [
+                      Text(
+                        "Detected location :",
+                        style: myTextStyleBase.size_C,
+                      ),
+                      SizedBox(height: 20),
+                      address == ""
+                          ? Text("Unknown")
+                          : Text(
+                              address,
+                              style: myTextStyleBase.size_C,
+                            ),
+                      SizedBox(height: 20),
+                      Text(
+                        DateFormat('EEE, MMM d').format(data.toLocal()),
+                        style: myTextStyleBase.size_B,
+                      ),
+                      SizedBox(height: 20),
+                      Text(DateFormat('h:mm a').format(data.toLocal()),
+                          style: myTextStyleBase.size_A),
+                      SizedBox(height: 20),
+                      Text(data.toLocal().timeZoneName,
+                          style: myTextStyleBase.size_B),
+                    ],
+                  ),
           ],
         ),
       ),
     );
   }
+}
+
+class myTextStyleBase {
+  static const size_A = TextStyle(fontSize: 50);
+  static const size_B = TextStyle(fontSize: 30);
+  static const size_C = TextStyle(fontSize: 15);
 }
